4b842fb2a159e8597c80395f4d347aee2d62c50c
Damir Svrtan <damir.svrtan@gmail.com>
2014-04-23 19:50:47 +0200

    Better rendering and rake test.

--- Rakefile
+++ Rakefile
@@ -1,2 +1,11 @@
 require "bundler/gem_tasks"
+require "rake/testtask"
 task default: :install
+
+
+Rake::TestTask.new do |t|
+  t.name = "test"  # this is the default
+  t.libs << "test"  # load the test dir
+  t.test_files = Dir['test/*test*.rb']
+  t.verbose = true
+end
--- lib/noodles/controller.rb
+++ lib/noodles/controller.rb
@@ -9,10 +9,20 @@ module Noodles
       @env = env
     end
 
-    def render(view_name, locals={})
-      filename = File.join 'app', 'views', "#{view_name}.html.erb"
+    def render(view_name)
+      filename = File.join 'app', 'views', controller_name, "#{view_name}.html.erb"
       template = File.read(filename)
-      Erubis::Eruby.new(template).result(locals.merge(env: env))
+      Erubis::Eruby.new(template).result(mapped_instance_variables)
     end
+
+    def controller_name
+      klass = self.class.to_s.gsub /Controller$/, ""
+      Noodles.to_underscore klass
+    end
+
+    def mapped_instance_variables
+      Hash[self.instance_variables.map {|var| [var, self.instance_variable_get(var)]}]
+    end
+
   end
 end
