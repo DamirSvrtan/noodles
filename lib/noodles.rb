c166fb59da3d1c68f63579918c5c57f5c69a39d6
Damir Svrtan <damir.svrtan@gmail.com>
2014-04-23 17:46:03 +0200

    ConstMissing added.

--- lib/noodles.rb
+++ lib/noodles.rb
@@ -1,4 +1,6 @@
 require "noodles/version"
+require "noodles/utils"
+require "noodles/dependencies"
 
 module Noodles
   class Application
@@ -6,7 +8,7 @@ module Noodles
       if env['PATH_INFO'] == '/favicon.ico'
         return [404, {'Content-Type' => 'text/html'}, []]
       elsif env['PATH_INFO'] == '/'
-        return [303, {'Location' => 'quotes/a_quote'}, []]
+        return [200, {'Content-Type' => 'text/html'}, ["<h1>Hello!</h1>"]]
       end
 
       klass, action = get_controller_and_action(env)
