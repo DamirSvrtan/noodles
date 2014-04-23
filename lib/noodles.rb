a3f808289c09de46d5a8eda01197f6bec1aebadb
Damir Svrtan <damir.svrtan@gmail.com>
2014-04-23 16:51:58 +0200

    Chapter2 w/ redirect finished.

--- lib/noodles.rb
+++ lib/noodles.rb
@@ -3,7 +3,31 @@ require "noodles/version"
 module Noodles
   class Application
     def call(env)
-      [200, {'Content-Type' => 'text/html'}, ["Hello from Ruby on Noodles!"]]
+      if env['PATH_INFO'] == '/favicon.ico'
+        return [404, {'Content-Type' => 'text/html'}, []]
+      elsif env['PATH_INFO'] == '/'
+        return [303, {'Location' => 'quotes/a_quote'}, []]
+      end
+
+      klass, action = get_controller_and_action(env)
+      controller = klass.new(env)
+      text = controller.send(action)
+      [200, {'Content-Type' => 'text/html'}, [text]]
+    end
+
+    def get_controller_and_action(env)
+        _, controller, action, after = env['PATH_INFO'].split('/', 4)
+      controller = controller.capitalize
+      controller += 'Controller'
+      [Object.const_get(controller), action]
     end
   end
+
+  class Controller
+    attr_reader :env
+    def initialize(env)
+      @env = env
+    end
+  end
+
 end
