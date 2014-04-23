a1232393fc1319912a09ba7de3f687d7dfc257c8
Damir Svrtan <damir.svrtan@gmail.com>
2014-04-23 14:21:55 +0200

    Back to loading from the dir. Version 0.0.2.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,6 @@ require 'rack/test'
 require 'test/unit'
 require 'pry'
 
-# this_dir = File.join(File.dirname(__FILE__), "..")
-# $LOAD_PATH.unshift File.expand_path(this_dir)
+this_dir = File.join(File.dirname(__FILE__), "..")
+$LOAD_PATH.unshift File.expand_path(this_dir)
 require "noodles"
