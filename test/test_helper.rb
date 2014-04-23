2e171b5ce5eadfdaa11b529a2ee1aa3ec62f56b6
Damir Svrtan <damir.svrtan@gmail.com>
2014-04-23 14:17:46 +0200

    Take from the gem.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,7 @@
 require 'rack/test'
 require 'test/unit'
+require 'pry'
 
-this_dir = File.join(File.dirname(__FILE__), "..")
-$LOAD_PATH.unshift File.expand_path(this_dir)
+# this_dir = File.join(File.dirname(__FILE__), "..")
+# $LOAD_PATH.unshift File.expand_path(this_dir)
 require "noodles"
