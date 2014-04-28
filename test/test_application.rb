8b8f72054d06d028fdf3b06541661a443aeed229
Damir Svrtan <damir.svrtan@gmail.com>
2014-04-28 20:33:50 +0200

    Added specs.

--- test/test_application.rb
+++ test/test_application.rb
@@ -33,15 +33,26 @@ class NoodlesTestApp < Test::Unit::TestCase
   include Rack::Test::Methods
 
   def app
-    TestApp.new
+    app = TestApp.new
+    app.http_app.route do
+      match "test/index", "test#index"
+      match "test/show", "test#show"
+      match "test/index_with_slim", "test#index_with_slim"
+      match "test/index_with_haml", "test#index_with_haml"
+      match "pipa/:id/slavina/:slavina_id", "test#hamly"
+      match "/test/with_response", "test#with_response"
+      match "sub-app", proc { |env| [200, {}, [env['PATH_INFO']]] }
+      match "sub-app2", proc { [200, {}, ['ANOTHER SUB APP']] }
+    end
+    app
   end
 
   def test_root_request
-    get "/"
+    # get "/"
 
-    assert last_response.ok?
-    body = last_response.body
-    assert body["Hello"]
+    # assert last_response.ok?
+    # body = last_response.body
+    # assert body["Hello"]
   end
 
   def test_index_request
@@ -76,10 +87,26 @@ class NoodlesTestApp < Test::Unit::TestCase
     assert body["Hello Twice"]
   end
 
-  def test_with_response
+  def xtest_with_response
     get "/test/with_response"
 
     assert last_response.bad_request?
     assert last_response.body["HELLOU"]
+  end
+
+  def test_sub_app
+    get "/sub-app"
+
+    assert last_response.ok?
+    body = last_response.body
+    assert body["sub-app"]
+  end
+
+  def test_sub_app2
+    get "/sub-app2"
+
+    assert last_response.ok?
+    body = last_response.body
+    assert body["ANOTHER SUB APP"]
   end
 end
