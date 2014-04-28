c01c6a4d3d16150e88e3cd70cdaaadc2adda1dcb
Damir Svrtan <damir.svrtan@gmail.com>
2014-04-28 19:47:56 +0200

    Simplified router working.

--- lib/noodles/http/routing.rb
+++ lib/noodles/http/routing.rb
@@ -1,88 +1,76 @@
-class RouteObject
-  def initialize
-    @rules = []
-  end
-  
-  def match(url, *args)
-    options = {}
-    options = args.pop if args[-1].is_a?(Hash)
-    options[:default] ||= {}
-    dest = nil
-    dest = args.pop if args.size > 0
-
-    raise "Too many args!" if args.size > 0
-
-    parts = url.split("/")
-    parts.select! { |p| !p.empty? }
-
-    vars = []
-    regexp_parts = parts.map do |part|
-      if part[0] == ":"
-        vars << part[1..-1]
-        "([a-zA-Z0-9]+)"
-      elsif part[0] == "*"
-        vars << part[1..-1]
-        "(.*)" 
-      else
-        part 
+module Noodles
+  module Http
+    class Router
+      def initialize
+        @routes = []
       end
-    end
-    regexp = regexp_parts.join("/")
-    
-    @rules.push({
-      :regexp => Regexp.new("^/#{regexp}$"),
-      :vars => vars,
-      :dest => dest,
-      :options => options,
-    })
-  end
-  
-  def check_url(url)
-    @rules.each do |r|
-      m = r[:regexp].match(url)
-      if m
-        options = r[:options]
-        params = options[:default].dup
-        r[:vars].each_with_index do |v, i|
-          params[v] = m.captures[i]
+      
+      def match(url, destination)
+        url_parts = url.split("/")
+        url_parts.select! { |p| !p.empty? }
+ 
+        path_params = []
+ 
+        regexp_parts = url_parts.map do |url_part|
+          if url_part[0] == ":"
+            path_params << url_part[1..-1]
+            "([a-zA-Z0-9]+)"
+          elsif url_part[0] == "*"
+            path_params << url_part[1..-1]
+            "(.*)" 
+          else
+            url_part 
+          end
         end
-        dest = nil
-        if r[:dest]
-          return get_dest(r[:dest], params)
-        else
-          controller = params["controller"]
-          action = params["action"]
-          return get_dest("#{controller}" + "##{action}", params)
+ 
+        regexp = regexp_parts.join("/")
+        
+        @routes.push regexp: Regexp.new("^/#{regexp}$"),
+                     path_params: path_params, 
+                     destination: destination, 
+                     url: url
+      end
+      
+      def check_url(url)
+        @routes.each do |r|
+          data_match = r[:regexp].match(url)
+          if data_match
+            params = {}
+            r[:path_params].each_with_index do |path_param, index|
+              params[path_param] = data_match.captures[index]
+            end
+            return get_destination(r[:destination], params)
+          end   
         end
-      end   
+        nil
+      end
+ 
+      def get_destination(destination, routing_params = {})
+        return destination if destination.respond_to?(:call)
+        if destination =~ /^([^#]+)#([^#]+)$/
+          name = $1.capitalize
+          cont = Object.const_get("#{name}Controller")
+          return cont.action($2, routing_params)
+        end
+        raise "No destination: #{destination.inspect}!"
+      end
+ 
     end
-    nil
   end
-
-  def get_dest(dest, routing_params = {})
-    return dest if dest.respond_to?(:call)
-    if dest =~ /^([^#]+)#([^#]+)$/
-      name = $1.capitalize
-      cont = Object.const_get("#{name}Controller")
-      return cont.action($2, routing_params)
-    end
-    raise "No destination: #{dest.inspect}!"
-  end
-
 end
-
-
+ 
+ 
 module Noodles
   module Http
     class Application
       def route(&block)
-        @route_obj ||= RouteObject.new
+        @route_obj ||= Router.new
         @route_obj.instance_eval(&block)
       end
       
       def get_rack_app(env)
-        raise "No routes!" unless @route_obj
-        @route_obj.check_url env["PATH_INFO"]
+        raise 'No routes!' unless @route_obj
+        @route_obj.check_url env['PATH_INFO']
       end
       # def get_controller_and_action(env)
       #   _, controller, action, after = env['PATH_INFO'].split('/', 4)
