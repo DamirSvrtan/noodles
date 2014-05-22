5a34916cb270d2606a157f591b28294f0b4388a4
Damir Svrtan <damir.svrtan@gmail.com>
2014-05-22 22:19:05 +0200

    [CLI] Listing relative paths of template_app.

--- cli/noodles_cli.rb
+++ cli/noodles_cli.rb
@@ -3,12 +3,32 @@ require 'pry'
 class NoodlesCLI < Thor
   desc "new app_name", "creates a boilerplate Noodles folder structure"
   def new(app_name)
-    FileUtils.mkdir(app_name, verbose: true)
-    FileUtils.cd(app_name) do
-      folders(app_name).each do |folder|
-        FileUtils.mkdir(folder,verbose: true)
-      end
+    # FileUtils.mkdir(app_name, verbose: true)
+    template_path = File.expand_path('../template', __FILE__)
+    wildcard_path = File.join(template_path, "**", "**")
+    # puts all_files
+    # binding.pry
+    all_paths = Dir[wildcard_path]
+
+    # first_path = all_paths.first
+
+    command_line_path = FileUtils.pwd
+
+    # binding.pry
+
+    # puts __FILE__
+    temp_path = Pathname.new(template_path)
+
+    all_paths.each do |string_path|
+      puts File.directory?(string_path)
+      path = Pathname.new string_path
+      puts path.relative_path_from(temp_path).to_s
     end
+    # FileUtils.cd(app_name) do
+    #   folders(app_name).each do |folder|
+    #     FileUtils.mkdir(folder,verbose: true)
+    #   end
+    # end
   end
 
   desc "start", "start the server"
