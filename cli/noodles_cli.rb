15207db5b3151a4081a97860b715b21fbc8ac590
Damir Svrtan <damir.svrtan@gmail.com>
2014-05-22 23:10:35 +0200

    Copying the files working.

--- cli/noodles_cli.rb
+++ cli/noodles_cli.rb
@@ -1,15 +1,20 @@
 require 'thor'
 require 'pry'
+require 'find'
+
 class NoodlesCLI < Thor
   desc "new app_name", "creates a boilerplate Noodles folder structure"
   def new(app_name)
     # FileUtils.mkdir(app_name, verbose: true)
-    template_path = File.expand_path('../template', __FILE__)
-    wildcard_path = File.join(template_path, "**", "**")
+    template_path = Pathname.new File.expand_path('../template', __FILE__)
+    # wildcard_path = Pathname.new File.join(template_path, "**", "**")
+
+
     # puts all_files
     # binding.pry
-    all_paths = Dir[wildcard_path]
+    all_paths = Find.find(template_path).map {|path| Pathname.new(path)}
 
+    all_paths = all_paths[1..-1]
     # first_path = all_paths.first
 
     command_line_path = FileUtils.pwd
@@ -17,13 +22,26 @@ class NoodlesCLI < Thor
     # binding.pry
 
     # puts __FILE__
-    temp_path = Pathname.new(template_path)
+    # temp_path = Pathname.new(template_path)
 
-    all_paths.each do |string_path|
-      puts File.directory?(string_path)
-      path = Pathname.new string_path
-      puts path.relative_path_from(temp_path).to_s
+    FileUtils.mkdir(app_name, verbose: true)
+
+    all_paths.each do |path|
+      # puts path.directory?
+      rel_path = path.relative_path_from(template_path).to_s
+      rel_path = File.join app_name, rel_path
+      # FileUtils.cd(app_name) do
+      if path.directory?
+        FileUtils.mkdir(rel_path, verbose: true)
+      else
+        File.open(rel_path, 'w') {|file| file.write(File.read(path))}
+      end
+      # end
+
+      # puts rel_path
+      # rel_path
     end
+    # binding.pry
     # FileUtils.cd(app_name) do
     #   folders(app_name).each do |folder|
     #     FileUtils.mkdir(folder,verbose: true)
