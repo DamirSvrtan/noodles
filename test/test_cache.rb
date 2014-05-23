6270af08e4dcad5c559317ac220465c1cc37c9ac
Damir Svrtan <damir.svrtan@gmail.com>
2014-05-23 23:24:05 +0200

    Test storing hashes in memcached.

--- test/test_cache.rb
+++ test/test_cache.rb
@@ -6,4 +6,9 @@ class SecretsTest < Minitest::Test
     Noodles.cache.set('user', 'Salvador')
     assert_equal 'Salvador', Noodles.cache.get('user')
   end
+
+  def test_cache_set_hash
+    Noodles.cache.set('X_session_id_X', user_id: 1)
+    assert_equal 1, Noodles.cache.get('X_session_id_X')[:user_id]
+  end
 end
