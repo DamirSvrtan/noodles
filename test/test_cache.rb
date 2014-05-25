6f0275f58c2899ea098280b294f026636a0ec5b8
Damir Svrtan <damir.svrtan@gmail.com>
2014-05-26 00:14:00 +0200

    Tests for nested attrs in memcached.

--- test/test_cache.rb
+++ test/test_cache.rb
@@ -19,4 +19,48 @@ class SecretsTest < Minitest::Test
     Noodles.cache.set('X_session_id_X', user_id: 1)
     assert_equal 1, Noodles.cache.get('X_session_id_X')[:user_id]
   end
+
+  def test_namespaced_cache
+    Noodles.cache.set(:sessions, {})
+
+    assert_equal Hash.new, Noodles.cache.get(:sessions)
+
+    sessions = Noodles.cache.get(:sessions)
+
+    damir_session = '312h0urq3'
+
+    damir_info = {user_id: 1, user_name: 'Damir'}
+
+    sessions[damir_session] = damir_info
+
+    Noodles.cache.set(:sessions, sessions)
+
+    assert_equal damir_session, Noodles.cache.get(:sessions).keys.first
+
+    assert_equal damir_info, Noodles.cache.get(:sessions)[damir_session]
+
+    sessions = Noodles.cache.get(:sessions)
+
+    jelena_session = 'AZ3981erQ2'
+
+    jelena_info = {user_id: 2, user_name: 'Jelena'}
+
+    sessions[jelena_session] = jelena_info
+
+    Noodles.cache.set(:sessions, sessions)
+
+    assert_equal jelena_info, Noodles.cache.get(:sessions)[jelena_session]
+
+    assert_equal 2, Noodles.cache.get(:sessions).count
+
+    sessions = Noodles.cache.get(:sessions)
+
+    sessions.delete(jelena_session)
+
+    Noodles.cache.set(:sessions, sessions)
+
+    assert_equal 1, Noodles.cache.get(:sessions).count
+
+    assert_equal damir_info, Noodles.cache.get(:sessions)[damir_session]
+  end
 end
